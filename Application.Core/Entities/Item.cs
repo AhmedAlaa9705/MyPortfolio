c69006a4165ee8b37e667c6485f3d0a9336fd11c
AhmedDev9705 <ahmeddev9705@gmail.com>
2023-02-15 17:50:40 +0200

    Add project files.

---
+++ Application.Core/Entities/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Core.Entities
+{
+    public class Item:EntityBase
+    {
+        public string ProjectName { get; set; }
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+    }
+}
